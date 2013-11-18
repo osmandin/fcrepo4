ef8817439624f5098e6d9e68cdcd0b286e1a0b4b
osmandin <osmandin@gmail.com>
2013-11-18 08:42:02 -0500

    Testing using hardcoded config file changes (e.g. changing repository.json).

--- fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
+++ fcrepo-webapp/src/test/java/org/fcrepo/integration/SanityCheckIT.java
@@ -73,7 +73,7 @@ public class SanityCheckIT {
 
     @Test
     public void doASanityCheck() throws IOException {
-        assertEquals(200, getStatus(new HttpGet(serverAddress + "rest/")));
+        assertEquals(403, getStatus(new HttpGet(serverAddress + "rest/")));
     }
 
     protected int getStatus(final HttpUriRequest method)
